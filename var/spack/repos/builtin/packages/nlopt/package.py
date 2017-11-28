3b82c0c0404218783db3048b59bd76de66eb79e3
shudson <shudson@anl.gov>
2017-11-28 12:36:58 -0600

    Update nlopt package

--- var/spack/repos/builtin/packages/nlopt/package.py
+++ var/spack/repos/builtin/packages/nlopt/package.py
@@ -6,7 +6,7 @@
 # Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
 # LLNL-CODE-647188
 #
-# For details, see https://github.com/llnl/spack
+# For details, see https://github.com/spack/spack
 # Please also see the NOTICE and LICENSE files for our notice and the LGPL.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -22,29 +22,12 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install nlopt
-#
-# You can edit this file again by typing:
-#
-#     spack edit nlopt
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
 
 
-class Nlopt(CMakePackage):
-#class Nlopt(AutotoolsPackage):    
-    """FIXME: Put a proper description of your package here."""
+class Nlopt(CMakePackage):  
+    """NLopt is a free/open-source library for nonlinear optimization, providing a common interface for a number of different free optimization routines available online as well as original implementations of various other algorithms."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://nlopt.readthedocs.io"
     url      = "https://github.com/stevengj/nlopt/releases/download/nlopt-2.4.2/nlopt-2.4.2.tar.gz"
 
@@ -59,8 +42,7 @@ class Nlopt(CMakePackage):
     
     #Note: matlab is licenced - spack does not download automatically
     variant("matlab", default=False, description="Build the Matlab bindings.")
-    
-    # FIXME: Add dependencies if required.
+
     depends_on('cmake@3.0:', type='build',when='@develop')
     depends_on('python', when='+python')
     depends_on('py-numpy', when='+python', type=('build', 'run'))
@@ -68,23 +50,6 @@ class Nlopt(CMakePackage):
     depends_on('guile', when='+guile')
     depends_on('octave', when='+octave')
     depends_on('matlab', when='+matlab')
-    
-    #Tarballs 2.4.2 and prev just use autotools - need to change class inheritance
-    #def configure_args(self):
-        ## FIXME: Add arguments other than --prefix
-        ## FIXME: If not needed delete the function
-        #spec = self.spec
-        #args = []
-        
-        #if '+python' in self.spec:
-            #args.extend([
-                #'PYTHON={0}'.format(spec['python'].command.path)
-                #])
-        
-        #if '+shared' in self.spec:
-            #args.extend(['--enable-shared'])
-            
-        #return args    
  
     def cmake_args(self):
         # Add arguments other than
@@ -92,12 +57,10 @@ class Nlopt(CMakePackage):
         spec = self.spec
         args = []
 
-        #Can just specify on command line to alter defaults - 
+        #Specify on command line to alter defaults: 
         #eg: spack install nlopt@develop +guile -octave +cxx
         
-        #Some do not need cmake options when using spack - spack will locate
-        
-        #Spack should locate python by default - but for consistent cmake options
+        #Spack should locate python by default - but to point to a build
         if '+python' in spec:
             args.append("-DPYTHON_EXECUTABLE=%s" % spec['python'].command.path)
         
@@ -112,4 +75,3 @@ class Nlopt(CMakePackage):
             args.append("-DMatlab_ROOT_DIR=%s" % spec['matlab'].command.path)
             
         return args
-    
